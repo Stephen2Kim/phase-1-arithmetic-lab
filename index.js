e789b29660896b176c469338b079566dd175b732
Stephen Muniu Kimani <stvnkimani@gmail.com>
2024-06-27 18:45:40 +0300

    Adds arithmetic changes in files

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = 0;
+random++;
+
+let num3 = 66
+let num4 = 31;
+let mod = num3 % num4;
+
+let numbers = [10, 5, 20, 15];
+let max = Math.max(...numbers);
